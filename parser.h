049ba1c3e3273bae85be3e7618061f91391beed4
Diana Laura Pérez Trujillo <108586028+Kitsulee@users.noreply.github.com>
2023-04-22 03:17:45 -0400

    Added Node structure (without funcions implem.)

--- parser.h
+++ parser.h
@@ -56,3 +56,62 @@ Token *tokenize(Source *source);
 /*Liberate the memory used by a Token*/
 void free_T(Token token);
 #endif
+
+#ifndef NODE_H
+#define NODE_H
+
+struct node
+{
+    /*Defining the type of node (if it's a command or a var)*/
+    enum node_type
+    {
+        N_COMAND,
+        N_VAR,
+    }
+
+    /*defining the type of var*/
+    enum val_type
+    {
+        V_SINT = 1,
+        V_UINT,
+        V_SLLONG,
+        V_ULLONG,
+        V_FLOAT,
+        V_DOUBLE,
+        V_CHAR,
+        V_STR,
+    }
+
+    /* Fix: Revisar bien esto*/
+    union value
+    {
+        long sint;
+        unsigned long uint;
+        long long sllong;
+        unsigned long long ullong;
+        double sfloat;
+        long double ldouble;
+        char chr;
+        char *str;
+    };
+    
+    struct node
+    {
+        enum node_type type;
+        enum val_type  val_type;
+        union value value;
+        int c_children;
+        struct node *first_child;
+        struct node *next_sibling;
+        struct node *previous_sibling;
+    }
+
+    typedef struct node Node;
+
+    Node *new_node(enum node_type type);
+    void add_child(Node *parent, Node *child);
+    void free_node(Node *node);
+    void set_node_val(NOde *node, char *val);
+}
+
+#endif
