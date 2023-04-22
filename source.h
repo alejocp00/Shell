60b5106d08efc5761595ae45be3484d27cf7503e
Diana Laura Pérez Trujillo <108586028+Kitsulee@users.noreply.github.com>
2023-04-22 12:15:52 -0400

    restructuring files

--- source.h
+++ source.h
@@ -32,31 +32,6 @@ void skip_blanks(Source *source);
 
 #endif
 
-
-#ifndef TOKEN_H
-#define TOKEN_H
-
-/*struct token will help us tokenizing*/
-struct token
-{
-    Source *source;
-    char *text;
-    long text_size;
-};
-
-/*Definining an alias for struct token*/
-typedef struct token Token;
-
-/*Extern token that indicates EOF*/
-extern Token EOF_token;
-
-/*This will retrieve next input token*/
-Token *tokenize(Source *source);
-
-/*Liberate the memory used by a Token*/
-void free_T(Token token);
-#endif
-
 #ifndef NODE_H
 #define NODE_H
 
--- token.c
+++ token.c
@@ -1,81 +1,7 @@
 #include <errno.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include "parser.h"
-
-#pragma region Source
-char point_next_char(Source *source)
-{
-    /*Verify if there is a source value or in_text value*/
-    if(!source || !source->in_text) 
-    {
-        errno = ENODATA; 
-        return 0;
-    }
-    
-    /*Verify if it has been initializated*/
-    if (source->position == INITIAL_S_POSITION)
-    {
-        source->position = -1;
-    }
-    /*Verify if it's the end of the source text*/
-    if(source->position >= source->size)
-    {
-        source->position = source->size;
-        return EOF;
-    }
-    source->position++;
-    return source->in_text[source->position];
-}
-
-char get_char(Source *source)
-{
-    /*Verify if there is a source value or a in_text value*/
-    if(!source || !source->in_text)
-    {
-        return 0;
-    }
-    /*Verify is it has been initializated*/
-    if(source->position == INITIAL_S_POSITION)
-    {
-        source->position = -1;
-    }
-
-    /*position of the next char*/
-    long pos = source->position + 1; 
-    
-    /*Verify if it's the end of the file*/
-    if(pos == source->size) 
-    {
-        return EOF;
-    }
-    
-    return source->in_text[pos];
-}
-
-void unget_char(Source *source)
-{
-    if(source->position < 0) return;
-    source->position--;
-}
-
-void skip_blanks(Source *source)
-{
-    /*Verify if there is a source value or a in_text value*/
-    if(!source || !source->in_text) return; 
-    
-    char aux;
-
-    while((aux = get_char(source)) != EOF && (aux == ' ' || aux == '\t')) 
-    {
-        point_next_char(source);
-    }
-}
-
-#pragma endregion
-
-#pragma region Token 
+#include "token.h"
+#include "source.h"
 
 /*Indicates the end of the input*/
 Token EOF_token = {
@@ -213,14 +139,4 @@ Token *tokenize(Source *source)
 
     token->source = source;
     return token;
-
-
-
-
-
-
 }
-
-
-#pragma endregion
-
