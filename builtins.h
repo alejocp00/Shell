817721fa31d59fa48057eb7831f3ce1e7bd8ff45
Diana Laura Pérez Trujillo <108586028+Kitsulee@users.noreply.github.com>
2023-04-25 15:12:25 -0400

    Added builtins structure

---
+++ builtins.h
@@ -0,0 +1,20 @@
+#ifndef BUILTINS_H
+#define BUILTINS_H
+
+/*Structure for helping manage the builtins*/
+typedef struct builtins
+{
+    /*builting's name*/
+    char *builtin_name;
+
+    /*function*/
+    int (*function)(int argc, char **argv);
+}Builtins;
+
+/*Array as storage of all the builtins*/
+extern Builtins builtins[];
+
+/*Amount of builtins*/
+int builtings_count;
+
+#endif
