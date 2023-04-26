1af97193e7af4942c93335d8d791e2900f5b95d7
Alejandro Camacho <69049973+alejocp00@users.noreply.github.com>
2023-04-26 18:39:29 -0400

    Created Stack and Queue behavior on DataStructure (#29)
    Created Stack and Queue behavior on DataStructure

---
+++ utils.h
@@ -0,0 +1,64 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "node.h"
+
+/**
+ * @brief Data structure for the data node. This node will be used in a linked list based implementation of a queue, or a stack.
+ *
+ */
+typedef struct data_node
+{
+    Node *value;
+    DataNode *next;
+} DataNode;
+
+/**
+ * @brief Create a new data node object
+ *
+ * @param value The value of the node
+ * @return DataNode* The new node
+ */
+DataNode *new_data_node(Node *value);
+
+#pragma region Stack methods
+
+/**
+ * @brief Add a new node to the stack
+ *
+ * @param stack The stack
+ * @param value The value of the node
+ */
+void push(DataNode *stack, Node *value);
+
+/**
+ * @brief Remove the first node of the queue
+ *
+ * @param queue The queue
+ * @return Node* The value of the node
+ */
+Node *pop(DataNode *stack);
+
+#pragma endregion
+
+#pragma region Queue methods
+
+/**
+ * @brief Push a new node to the queue
+ *
+ * @param queue The queue
+ * @param value The value of the node
+ */
+void enqueue(DataNode *queue, Node *value);
+
+/**
+ * @brief Pop the first node of the stack
+ *
+ * @param stack The stack
+ * @return Node* The value of the node
+ */
+Node *dequeue(DataNode *queue);
+
+#pragma endregion
+
+#endif
