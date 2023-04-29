4c26a9c823c4e518967ba4b6ede8cb2cc6308f02
Alejandro Camacho <69049973+alejocp00@users.noreply.github.com>
2023-04-29 12:25:02 -0400

    Builtins branch (#34)
    * Added builtins structure
    
    * fixed a var
    
    * Added help files
    
    * added help.c
    
    * Added comments on help.c
    
    * Added history file
    
    * added history.txt
    
    * auxiliar_commit
    
    * Builtins branch arturo (#33)
    
    * cd done and redirect
    
    * creating pipes
    
    * redirects almost done
    
    * working in background and fixing pipes
    
    * temp
    
    * Refactoring
    
    ---------
    
    Co-authored-by: ArturoMetalhead <perezcabreracarlosarturo@gmail.com>
    
    ---------
    
    Co-authored-by: Diana Laura Pérez Trujillo <108586028+Kitsulee@users.noreply.github.com>
    Co-authored-by: ArturoMetalhead <perezcabreracarlosarturo@gmail.com>

---
+++ background.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include "builtins.h"
+#include "node.h"
+#include "list.h"
+
+list *background_process;
+
+/**
+ * @brief This method excecute the & function
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int background_func(int argc, char **argv)
+{
+    pid_t pid;
+    pid = fork(); 
+    PushEnd(background_process, pid);//inserting the pid in the array
+
+    if (pid == 0) { //Child process
+       // printf("Children process executing...\n");
+        execvp("programa", NULL); // Ejecutar programa en segundo plano////////////////
+    } else if (pid > 0) { // Parent process
+       // printf("parent process executing...\n");
+       // waitpid(pid, NULL, WNOHANG); // wait for child to exit
+       // printf("child process finished.\n");
+    } else { //error ocurred
+        printf("error to create child process.");
+        return 1;
+    }
+    return 0;
+}
+Builtins background_func_struct = {"&", background_func};
+
+
+/**
+ * @brief This method excecute the jobs function
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int jobs(int argc, char **argv)
+{ 
+     pid_t pid=getpid();
+     pid_t pgid= getpgid(pid);
+    printf("Background processes:\n");
+    pid_t p;
+    // iterates over all possible process IDs
+    for (p = 1; p <= 65535; p++) {//65535 its the max number of process
+        // obtain the group ID for the process
+        pgid = getpgid(pid);
+
+        // if the pid its different from the group id, then its a background process
+        if (getpgid(p) == pgid && p != pid) {
+            printf("PID=%d",  p);
+        }
+    }
+    return 0;
+}
+Builtins jobs_struct = {"jobs", jobs};
+
+/**
+ * @brief This method excecute the fg function
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int fg(int argc, char **argv)
+{
+    if(argc==NULL){//Hacer que se ejecute el proceso mas reciente enviado al background///////////
+     pid_t recentProcess= getRecent(background_process);
+     //exec("fg", recentProcess);
+     kill(recentProcess, SIGCONT);
+     waitpid(recentProcess, NULL, 0);
+    }
+    else{
+    // send SIGCONT signal to the process
+    kill(argc, SIGCONT);
+      // wait for the process to finish
+    waitpid(argc, NULL, 0);
+    }
+  
+}
+Builtins fg_struct = {"fg", fg};
+
+/**
+ * @brief This method excecute the bg function
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+
+void my_sh_update_background() {
+    int status;
+
+    if (background_process->size > 0) {
+        for (int i = 0; i < background_process->size; ++i) {
+            waitpid(GetValue(background_process,i), &status, WNOHANG);
+            if (WIFEXITED(status)) {
+                printf("[%d]\tDone\t%d\n", i + 1, GetValue(background_process,i));
+                DeleteValue(background_process, i);
+                i = -1;
+            }
+        }
+    }
+}
---
+++ help.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+
+/*print char by char the content of the text file indicated*/
+int print_file_text(FILE *file)
+{
+    int c = 0;
+    while((c = fgetc(file)) != EOF)
+    {
+        printf("%c", c);
+    }
+    return 0;
+}
+
+/*help function indicates and prints which file to read depending on the keyword*/
+int help(char *keyword)
+{
+    FILE *file;
+    int c = 0;
+    /*if keyword is NULL, then it prints generalhelp file*/
+    if(!keyword)
+    {
+        file = fopen("help/generalhelp.txt", "r");
+    }
+    else
+    {
+       char buffer[100];
+       strcat(strcpy(buffer, "help/"), keyword);
+       strcat(buffer, ".txt");
+       file = fopen(buffer, "r");
+       
+       /*if file is NULL it means that don't exist a file with the keyword indicated*/
+       if(!file)
+       {
+        printf("Keyword no válida \n");
+        return 1;
+       }
+      
+    }
+     print_file_text(file);
+       fclose(file);
+       return 0;
+}
