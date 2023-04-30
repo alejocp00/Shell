67245495c309dd8ab6b2e0a803e0e5950e68887b
Diana Laura Pérez Trujillo <108586028+Kitsulee@users.noreply.github.com>
2023-04-30 19:15:06 -0400

    added true_false.c, deleted true.c and false.c

--- built-ins/true.c
+++
@@ -1,6 +0,0 @@
-#include "builtins.h"
-
-int true(int argc, char **argv)
-{
-    return 0;
-}
--- built-ins/true_false.c
+++ built-ins/true_false.c
@@ -1,5 +1,10 @@
 #include "builtins.h"
 
+int true(int argc, char **argv)
+{
+    return 0;
+}
+
 int false(int argc, char **argv)
 {
     return 1;
