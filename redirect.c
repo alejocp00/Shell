cd080c35e65b1ac7890c886ffdd1083736d4e400
Alejandro Camacho <alex00cu@gmail.com>
2023-04-30 16:44:34 -0400

    Enchantments

--- redirect.c
+++ redirect.c
@@ -3,38 +3,39 @@
 #include "builtins.h"
 
 /**
- * @brief This method excecute the > function
- * 
- * @param argc 
- * @param argv 
- * @return int 
+ * @brief This method execute the > function
+ *
+ * @param argc
+ * @param argv
+ * @return int
  */
 int retofile(int argc, char **argv)
 {
-    FILE *fp;
-        fp = fopen(argv[1], "w+");
-        fprintf(fp, "%s", argv[0]);
-        fclose(fp);
+  FILE *fp;
+  fp = fopen(argv[1], "w+");
+  fprintf(fp, "%s", argv[0]);
+  fclose(fp);
 }
 Builtins retofile_struct = {">", retofile};
 /**
- * @brief This method excecute the >> function
- * 
- * @param argc 
- * @param argv 
- * @return int 
+ * @brief This method execute the >> function
+ *
+ * @param argc
+ * @param argv
+ * @return int
  */
 int retofileap(int argc, char **argv)
 {
-    FILE *fp;
+  FILE *fp;
 
   fp = fopen(argv[1], "a");
 
-  if (fp == NULL) {
+  if (fp == NULL)
+  {
     return 1;
   }
 
-  fprintf(fp, argv[0]);
+  // fprintf(fp, argv[0]);
 
   fclose(fp);
 
@@ -42,19 +43,20 @@ int retofileap(int argc, char **argv)
 }
 Builtins retofileap_struct = {">>", retofileap};
 /**
- * @brief This method excecute the < function
- * 
- * @param argc 
- * @param argv 
- * @return int 
+ * @brief This method execute the < function
+ *
+ * @param argc
+ * @param argv
+ * @return int
  */
-int refromfile(int argc, char **argv)//como entrarselo a la otra funcion
+int refromfile(int argc, char **argv) // como entrarselo a la otra función
 {
   FILE *fp;
 
   fp = fopen(argv[1], "r");
 
-  if (fp == NULL) {
+  if (fp == NULL)
+  {
     return 1;
   }
 
