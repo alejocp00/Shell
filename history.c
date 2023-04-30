f6a6efbe202e6daf4f699d882a3a33b5fcd9fbe4
Diana Laura Pérez Trujillo <108586028+Kitsulee@users.noreply.github.com>
2023-04-30 00:20:19 -0400

    fixed history

--- history.c
+++ history.c
@@ -3,8 +3,41 @@
 
 //(sin terminar)
 
+/*Add to the history file the command*/
 int add_to_history(char *command)
 {
   FILE *file = fopen("history/history.txt", "a");
+  rewind(file);
   fputs(command, file);
+}
+
+/*prints 10 last commands*/
+int history(int argc, char **argv)
+{
+  /*count of the lines*/
+  int count = 0;
+
+  /*max lines of history that it will showed*/
+  int max_lines = 10;
+
+  /*stores the string of each line to read*/
+  char text_string[1030];
+
+  FILE *file = fopen("aux.txt", "a+");
+
+  if(!file) return 1;
+
+  /*puts the cursor at the beginning of the file*/
+  rewind(file);
+
+  while(count < max_lines)
+  {
+    /*takes the corresponding line and stores it in the text_string variable*/
+    fgets(text_string,1030,file);
+    
+    printf("%d %s\n", count+1, text_string);
+    count++;
+  }
+  fclose(file);
+  return 0;
 }
